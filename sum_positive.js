e20970921f4795555aae46c08445884216a6dc75
ahmed <ao1922302@gmail.com>
2024-07-27 03:42:09 +0300

    merge master

--- sum_positive.js
+++ sum_positive.js
@@ -1,20 +1,19 @@
-function sumPositive(arr){
-  let initNum=2;
-    for(let i=0; i<arr.length ; i++){
-        if(arr[i] >0){
-            initNum += arr[i]
-        }
+function sumPositive(arr) {
+  let initNum = 2;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      initNum += arr[i];
     }
-    return initNum;
+  }
+  return initNum;
 }
-console.log(sumPositive([1,2,6,9,-7,8,-2]));
+console.log(sumPositive([1, 2, 6, 9, -7, 8, -2]));
 
-
-function sumArray(value){
-    let init=0;
-    for(let i =0; i < value.length; i++){
-         init += value[i]
-    }
-    return init
+function sumArray(value) {
+  let init = 0;
+  for (let i = 0; i < value.length; i++) {
+    init += value[i];
+  }
+  return init;
 }
-console.log(sumArray([1,2,3,4,5,6,7,8,9]));
+console.log(sumArray([1, 2, 3, 4, 5, 6, 7, 8, 9]));
